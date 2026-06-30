4166b2d01c1234ff369c2771ed26fd8a7d7559bd
alissonbalbuenap-oss <alissonbalbuenap@gmail.com>
2026-06-30 06:46:02 -0500

    Add files via upload

---
+++ server.js
@@ -0,0 +1,88 @@
+const express = require("express");
+const WebSocket = require("ws");
+const db = require("./firebase-admin");
+
+const app = express();
+
+const servidor = app.listen(3000, () => {
+    console.log("Servidor iniciado en el puerto 3000");
+});
+
+const wss = new WebSocket.Server({
+    server: servidor
+});
+
+wss.on("connection", (ws) => {
+
+    console.log("Cliente conectado");
+
+    ws.on("message", async (mensaje) => {
+
+        try {
+
+            const datos = JSON.parse(mensaje.toString());
+
+            console.log("Mensaje recibido:", datos);
+
+            if(datos.tipo === "RFID"){
+
+                await db
+                    .collection("usuarios")
+                    .doc(datos.uid)
+                    .collection("practicas")
+                    .add({
+
+                        fecha: new Date(),
+                        timestamp: Date.now(),
+
+                        tipo: "RFID",
+                        modo: datos.modo,
+
+                        duracion: datos.duracion,
+                        aciertos: datos.aciertos,
+                        errores: datos.errores,
+
+                        erroresDetalle: datos.erroresDetalle || {},
+
+                        totalPreguntas: datos.aciertos + datos.errores,
+
+                        porcentaje:
+                        (datos.aciertos + datos.errores) === 0
+                        ? 0
+                        : Math.round(
+                            datos.aciertos /
+                            (datos.aciertos + datos.errores) * 100
+                        ),
+
+                        dispositivo: "ESP32",
+                        version: "1.0"
+
+                    });
+
+                console.log("Práctica RFID guardada en Firestore");
+
+                ws.send(JSON.stringify({
+                    estado: "ok",
+                    mensaje: "Practica guardada"
+                }));
+
+            }
+
+        } catch(error) {
+
+            console.error("Error:", error);
+
+            ws.send(JSON.stringify({
+                estado: "error",
+                mensaje: error.message
+            }));
+
+        }
+
+    });
+
+    ws.on("close", () => {
+        console.log("Cliente desconectado");
+    });
+
+});
